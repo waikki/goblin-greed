1b380a2fce27de473bef9da35206485145188002
Maikki <maikki.leppaaho@gmail.com>
2015-03-07 16:31:06 +0200

    Estettiin pelaajan liikkuminen syvyyssuunnassa

--- tasoloikkaPeli/Assets/Script/SimpleER_Controller.cs
+++ tasoloikkaPeli/Assets/Script/SimpleER_Controller.cs
@@ -7,11 +7,13 @@ public class SimpleER_Controller : MonoBehaviour {
 	public float speed = 6.0F;
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
+	public float zPull = 1;
 	Vector3 moveDirection = Vector3.zero;
+	private float zPosition;
 
 	void Start()
 	{
-
+		zPosition = transform.position.z;
 	}
 	
 	void Update() 
@@ -24,26 +26,32 @@ public class SimpleER_Controller : MonoBehaviour {
 
 		CharacterController controller = GetComponent<CharacterController> ();
 		
-			if (controller.isGrounded) 
-			{
+		if (controller.isGrounded) 
+		{
 
-				moveDirection = new Vector3 (-1f, 0f, 0f);
-				
-				moveDirection = transform.TransformDirection (moveDirection);
-				
-				moveDirection *= speed;
-				//sama kuin moveDirection = moveDirection * speed;
-				
-				if (Input.GetButton ("Jump"))
-				{
-					moveDirection.y = jumpSpeed;
-					
-				}	
-
-			}
+			moveDirection = new Vector3 (-1f, 0f, 0f);
 			
-			moveDirection.y -= gravity * Time.deltaTime;
-			controller.Move (moveDirection * Time.deltaTime);
+			moveDirection = transform.TransformDirection (moveDirection);
+			
+			moveDirection *= speed;
+			//sama kuin moveDirection = moveDirection * speed;
+			
+			if (Input.GetButton ("Jump"))
+			{
+				moveDirection.y = jumpSpeed;
+				
+			}	
+
+		}
+
+		if(transform.position.z != zPosition)
+		{
+			moveDirection.z = (zPosition - transform.position.z) * zPull;
+		}
+		
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move (moveDirection * Time.deltaTime);
+		//transform.position = new Vector3 (transform.position.x, transform.position.y, positionZ);
 
 	}
 	
