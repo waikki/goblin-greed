7c3fcea92ea6bd55e05e479e186c1eafe910c7ad
Maikki Lepp�aho <maikki.leppaaho@gmail.com>
2015-03-19 11:17:19 +0200

    Lisätty starttinappula ja kuoleminen
    Kenttä alkaa alusta, jos pelaaja tippuu ruudun ulkopuolelle.

---
+++ tasoloikkaPeli/Assets/Script/DeathTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.tag == "Player") {
+			Debug.Log("Collided with " + collider.name);
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+}
---
+++ tasoloikkaPeli/Assets/Script/StartButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0;
+	}
+	
+	public void StartGame() {
+		Time.timeScale = 1;
+		Destroy (gameObject);
+	}
+}
